422b7667898d50a1500e5286f16102f13e8edb4c
Lobanov <valerijlobanov8891@gmail.com>
2024-07-02 11:34:06 +0300

    add logger

--- server/src/app.js
+++ server/src/app.js
@@ -3,23 +3,26 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const swaggerUI = require("swagger-ui-express");
-
+const cookieParser = require('cookie-parser');
 const {swaggerSpecification} = require ("./services/svagger")
 const productsRouter = require("./routes/products/products.router");
 const categoriesRouter = require("./routes/categories/categories.router");
 const ordersRouter = require("./routes/orders/orders.router");
+const { logger } = require("./services/logger");
+const { errorHandler } = require('./services/errorhandler');
 
 const app = express();
 
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpecification));
-
+app.use(logger);
 app.use(
   cors({
     origin: "http://localhost:3000",
+    credentials: true
   })
 );
-
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use("/products", productsRouter);
@@ -29,5 +32,7 @@ app.use("/orders", ordersRouter);
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
+
+app.use(errorHandler);
 
 module.exports = app;
